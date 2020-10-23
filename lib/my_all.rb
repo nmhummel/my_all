c16c48053e8f3e07fc6973f89f270d108996e8b9
nmhummel <natalie.hummel@gmail.com>
2020-10-23 14:44:52 -0500

    commit tree ecb0d5c1edc920ba8781991df440a32b7ae1d29d

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,22 @@
-require 'pry'
+
 
 def my_all?(collection)
+    i = 0
+    block_return_values = []
+    while i < collection.length
+      block_return_values << yield(collection[i])
+      i = i + 1
+    end
+  
+    if block_return_values.include?(false)
+      false
+    else
+      true
+    end
+end 
 
-end
+
+
+#yield1
+my_all?([1,2,3]) {|i| i < 2} 
+# collection of 1,2,3 - each number assessed and all must meet criteria of being less than 2
